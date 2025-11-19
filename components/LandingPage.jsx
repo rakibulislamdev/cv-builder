9e4aa75cd4ff584da1c465bca15c0d813a63a34a
Rakibulislam1 <rirakib03@gmail.com>
2025-11-19 12:31:13 +0600

    css issue fix

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
 
     return (
         <div className="bg-white py-12 md:py-20 lg:py-24">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl min-h-screen flex justify-center items-center mx-auto px-4 sm:px-6 lg:px-8">
 
 
                 <div className="flex flex-col md:flex-row items-center">
